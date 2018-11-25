9201382fb1a4aec35241c3b3d5098cce61933b11
chenxin0215@gmail.com <chenxin0215@gmail.com>
2018-11-25 21:53:52 +0800

    all pras

---
+++ prc/monoStack/monoStack/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace monoStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testList = new List<int>() { 4, 6, 3, 2, 5, 2, 7 };
+
+            var listIdx = FindthelessValuetoLeftdirection(testList);
+
+
+        }
+
+        public static List<int> FindthelessValuetoLeftdirection(List<int> testList)
+        {
+            var stackUse = new Stack<int>();
+            var listIdx = new List<int>();
+            for (var i = 0; i < testList.Count; i++)
+            {
+                while (stackUse.Count > 0 && testList[stackUse.Peek()] >= testList[i])
+                {
+                    stackUse.Pop();
+                }
+                if (stackUse.Count == 0)
+                {
+                    listIdx.Add(99);
+                }
+                else
+                {
+                    listIdx.Add(stackUse.Peek());
+                }
+
+                stackUse.Push(i);
+            }
+
+            return listIdx;
+        }
+
+
+
+
+    }
+
+
+
+
+}
---
+++ prc/quickSort/quickSort/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quickSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arrayData  = new  int[] {6,3,1,4,2,5,9};
+
+            var pivot = partition(0, 6, arrayData);
+
+            quickSortMethod(arrayData);
+
+        }
+
+        public static void quickSortMethod(int[] array)
+        {
+            if (array.Count() > 1)
+            {
+                var pivot = partition(0, array.Count() - 1, array);
+                var subArray2 = array.Skip(pivot + 1).ToArray();
+                var subArray1 = array.Take(pivot).ToArray();
+
+                quickSortMethod(subArray1);
+                quickSortMethod(subArray2);
+            }
+            else
+            {
+                return; 
+            }
+
+        }
+
+
+        public static int partition(int left, int right, int[] array)
+        {
+            int pivot = left;
+            while (right > left)
+            {
+                while (right>pivot)
+                {
+                    if (array[right]<array[pivot])
+                    {                                     
+                        var a = array[pivot];
+                        array[pivot] = array[right];
+                        array[right] = a;
+                        pivot = right;
+
+                        break;
+                    }
+                    right--;
+                }
+
+                while (left<pivot)
+                {
+                    if (array[pivot] < array[left])
+                    {
+                        var a = array[pivot];
+                        array[pivot] = array[left];
+                        array[left] = a;
+
+                        pivot = left;
+
+                        break;
+                    }
+                    left++;
+                }
+
+            }
+            return pivot; 
+        }
+
+        public static void Swap(int a, int b)
+        {
+            var temp = a;
+            a = b;
+            b = temp;
+
+        }
+
+
+
+    }
+}
